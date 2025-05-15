386a7ca898a3ce967e00b0e23b74e4e63dcb9e4d
maliqt <maliq.tepkhoom@wsu.edu>
2025-05-14 21:22:40 -0700

    Working on a simple JavaScript Drum Kit when pressing corresponding keyboard keys.

---
+++ DrumKit/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('keydown', function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = document.querySelector(`.keys li[data-key="${e.key}"]`);
+
+    if(!audio) return;
+    console.log(audio);
+    console.log(key);
+
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+});
